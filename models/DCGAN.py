d77338388aecaff595d2f75081cff0adf6643df7
MaroonBlue <martyna.majchrzak19@gmail.com>
2023-06-12 16:45:30 +0200

    DCGAN 128x128

--- models/DCGAN.py
+++ models/DCGAN.py
@@ -11,6 +11,42 @@ def weights_init(m):
         nn.init.constant_(m.bias.data, 0)
 
 # Generator Code
+# Root directory for dataset
+#dataroot = "data/sample/data0/lsun/bedroom/"
+
+# Number of workers for dataloader
+#workers = 2
+
+# Batch size during training
+#batch_size = 128
+
+# Spatial size of training images. All images will be resized to this
+#   size using a transformer.
+#image_size = 256
+
+# Number of channels in the training images. For color images this is 3
+#nc = 3
+
+# Size of z latent vector (i.e. size of generator input)
+#nz = 100
+
+# Size of feature maps in generator
+#ngf = 256
+
+# Size of feature maps in discriminator
+#ndf = 256
+
+# Number of training epochs
+#num_epochs = 5
+
+# Learning rate for optimizers
+#lr = 0.0002
+
+# Beta1 hyperparameter for Adam optimizers
+#beta1 = 0.5
+
+# Number of GPUs available. Use 0 for CPU mode.
+#ngpu = 0
 
 class Generator(nn.Module):
     def __init__(self, ngpu, nz, ngf, nc):
@@ -41,6 +77,39 @@ class Generator(nn.Module):
 
     def forward(self, input):
         return self.main(input)
+
+class _netG(nn.Module):
+    def __init__(self, ngpu, nz, ngf, nc):
+        super(_netG, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(     nz, ngf * 16, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 16),
+            nn.ReLU(True),
+            # state size. (ngf*16) x 4 x 4
+            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 8 x 8
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 16 x 16 
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 32 x 32
+            nn.ConvTranspose2d(ngf * 2,     ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 64 x 64
+            nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 128 x 128
+        )
+    def forward(self, input):
+        return self.main(input)
     
 class Discriminator(nn.Module):
     def __init__(self, ngpu, nc, ndf):
@@ -67,5 +136,37 @@ class Discriminator(nn.Module):
             nn.Sigmoid()
         )
 
+    def forward(self, input):
+        return self.main(input)
+    
+class _netD(nn.Module):
+    def __init__(self, ngpu, nc, ndf):
+        super(_netD, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is (nc) x 128 x 128
+            nn.Conv2d(nc, ndf, 4, stride=2, padding=1, bias=False), 
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 64 x 64
+            nn.Conv2d(ndf, ndf * 2, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 32 x 32
+            nn.Conv2d(ndf * 2, ndf * 4, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 16 x 16 
+            nn.Conv2d(ndf * 4, ndf * 8, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 8 x 8
+            nn.Conv2d(ndf * 8, ndf * 16, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf * 16),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*16) x 4 x 4
+            nn.Conv2d(ndf * 16, 1, 4, stride=1, padding=0, bias=False),
+            nn.Sigmoid()
+            # state size. 1
+        )
     def forward(self, input):
         return self.main(input)
